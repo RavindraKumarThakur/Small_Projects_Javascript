532485d8df45fee753812f91ed27abe462bec807
ravindra_thakur <kumarravindra54787@gmail.com>
2024-03-12 03:47:16 +0530

    Fixed enter bug

--- Notes_App/app.js
+++ Notes_App/app.js
@@ -4,13 +4,13 @@ let allNotes = document.querySelectorAll('.content');
 let newContainer;
 let deleteIcon;
 
-console.log(localStorage.getItem('Data'));
 showData();
+console.log(localStorage.getItem('Data'));
 
 createBtn.addEventListener('click',() => {
     newContainer = document.createElement('p');
     newContainer.classList.add('content');
-    newContainer.setAttribute('contenteditable','true');
+    newContainer.contentEditable = 'true';
     deleteIcon = document.createElement('img');
     deleteIcon.classList.add('delete')
     deleteIcon.src = 'images/delete.png';
@@ -27,7 +27,11 @@ notesContainer.addEventListener('click',function(e){
     }
 })
 
-notesContainer.addEventListener('keyup',() => {
+notesContainer.addEventListener('keydown',(e) => {
+    if(e.key === 'Enter'){
+        document.execCommand('insertLineBreak');
+        e.preventDefault();
+    }
     saveData();
     console.log(localStorage.getItem('Data'));
 })
@@ -38,5 +42,6 @@ function saveData(){
 saveData();
 
 function showData(){
-    notesContainer.innerHTML = localStorage.getItem('Data');
+    notesContainer.innerHTML  = localStorage.getItem('Data');
+
 }
